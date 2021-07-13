SexualReptilians/opentk

commit 46bdb59a662c3870fd3cea468dac3b20072669c8

Also pass UseVirtualKeys to WinRawKeyboard, as bool

src/OpenTK/Platform/Windows/WinFactory.cs
@@ -80,6 +80,7 @@ namespace OpenTK.Platform.Windows
 
         public override INativeWindow CreateNativeWindow(int x, int y, int width, int height, string title, GraphicsMode mode, GameWindowFlags options, DisplayDevice device)
         {
+            RawInputDriver.UseVirtualKeys = (options & GameWindowFlags.UseVirtualKeys) == GameWindowFlags.UseVirtualKeys;
             return new WinGLNative(x, y, width, height, title, options, device);
         }
 

src/OpenTK/Platform/Windows/WinRawInput.cs
@@ -37,6 +37,7 @@ namespace OpenTK.Platform.Windows
         private WinRawKeyboard keyboard_driver;
         private WinRawMouse mouse_driver;
         private WinRawJoystick joystick_driver;
+        private bool use_virtual_keys;
 
         private IntPtr DevNotifyHandle;
         private static readonly Guid DeviceInterfaceHid = new Guid("4D1E55B2-F16F-11CF-88CB-001111000030");
@@ -126,7 +127,7 @@ namespace OpenTK.Platform.Windows
 
         protected override void CreateDrivers()
         {
-            keyboard_driver = new WinRawKeyboard(Parent.Handle);
+            keyboard_driver = new WinRawKeyboard(Parent.Handle) {UseVirtualKeys = use_virtual_keys};
             mouse_driver = new WinRawMouse(Parent.Handle);
             joystick_driver = new WinRawJoystick(Parent.Handle);
             DevNotifyHandle = RegisterForDeviceNotifications(Parent);
@@ -161,6 +162,16 @@ namespace OpenTK.Platform.Windows
             }
             Functions.GetRawInputDeviceList(ridl, ref count, API.RawInputDeviceListSize);
             return ridl;
+        }
+
+        public bool UseVirtualKeys
+        {
+            set
+            {
+                // update driver if it already exists
+                if (keyboard_driver != null) keyboard_driver.UseVirtualKeys = value;
+                use_virtual_keys = value;
+            }
         }
 
         public override IKeyboardDriver2 KeyboardDriver

src/OpenTK/Platform/Windows/WinRawKeyboard.cs
@@ -42,6 +42,8 @@ namespace OpenTK.Platform.Windows
         private readonly Dictionary<ContextHandle, int> rawids = new Dictionary<ContextHandle, int>();
         private readonly IntPtr window;
         private readonly object UpdateLock = new object();
+        
+        public bool UseVirtualKeys { private get; set; }
 
         public WinRawKeyboard(IntPtr windowHandle)
         {
@@ -187,7 +189,9 @@ namespace OpenTK.Platform.Windows
                 int keyboard_handle = rawids.ContainsKey(handle) ? rawids[handle] : 0;
                 keyboard = keyboards[keyboard_handle];
 
-                Key key = WinKeyMap.TranslateKey(scancode, vkey, extended0, extended1, out is_valid);
+                Key key = UseVirtualKeys ?
+                    WinKeyMap.TranslateVKey(scancode, vkey, extended0, out is_valid) :
+                    WinKeyMap.TranslateKey(scancode, vkey, extended0, extended1, out is_valid);
 
                 if (is_valid)
                 {

commit 110978ef53f233b9c727a8d968bcde6ec1d0588c

Fixed RawInputDriver getter being triggered prematurely
  Caused by previous commit 46bdb59a

src/OpenTK/Platform/Windows/WinFactory.cs
@@ -40,6 +40,8 @@ namespace OpenTK.Platform.Windows
         // that they call WinFactory methods internally.
         private WinRawInput rawinput_driver; // For keyboard and mouse input
 
+        private bool use_virtual_keys;
+
         internal static IntPtr OpenGLHandle { get; private set; }
         private const string OpenGLName = "OPENGL32.DLL";
 
@@ -80,7 +82,11 @@ namespace OpenTK.Platform.Windows
 
         public override INativeWindow CreateNativeWindow(int x, int y, int width, int height, string title, GraphicsMode mode, GameWindowFlags options, DisplayDevice device)
         {
-            RawInputDriver.UseVirtualKeys = (options & GameWindowFlags.UseVirtualKeys) == GameWindowFlags.UseVirtualKeys;
+            use_virtual_keys = (options & GameWindowFlags.UseVirtualKeys) == GameWindowFlags.UseVirtualKeys;
+            if (rawinput_driver != null)
+            {
+                rawinput_driver.UseVirtualKeys = use_virtual_keys;
+            }
             return new WinGLNative(x, y, width, height, title, options, device);
         }
 
@@ -136,6 +142,7 @@ namespace OpenTK.Platform.Windows
                     if (rawinput_driver == null)
                     {
                         rawinput_driver = new WinRawInput();
+                        rawinput_driver.UseVirtualKeys = use_virtual_keys;
                     }
                     return rawinput_driver;
                 }